f53476b9865a3bd049c426fcaaa9cb71429588cd
jonas060831 <jonas_sulit2001@yahoo.com>
2020-05-20 22:33:18 -0700

    added image insets extensions

---
+++ Sources/JSwiftTools/Extensions/Image+Insets.swift
@@ -0,0 +1,27 @@
+//
+//  Image+Insets.swift
+//  JSwiftToolsEDITOR
+//
+//  Created by iAmJOnAs on 5/20/20.
+//  Copyright © 2020 iAmJOnAs. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+    func imageWithInsets(insetDimen: CGFloat) -> UIImage {
+        return imageWithInset(insets: UIEdgeInsets(top: insetDimen, left: insetDimen, bottom: insetDimen, right: insetDimen))
+    }
+    
+    func imageWithInset(insets: UIEdgeInsets) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(
+            CGSize(width: self.size.width + insets.left + insets.right,
+                   height: self.size.height + insets.top + insets.bottom), false, self.scale)
+        let origin = CGPoint(x: insets.left, y: insets.top)
+        self.draw(at: origin)
+        let imageWithInsets = UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(self.renderingMode)
+        UIGraphicsEndImageContext()
+        return imageWithInsets!
+    }
+    
+}
